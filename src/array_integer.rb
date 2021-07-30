26a67abb1493a6d0878b3212b1239473f36dd929
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-07-30 16:54:39 +0700

    input [9,9] return [1,0,0] test & logic

--- src/array_integer.rb
+++ src/array_integer.rb
@@ -1,12 +1,23 @@
 class ArrayInteger
     def transform(int)
-        last_index = int.length - 1
-        int[last_index] = int[last_index] + 1
-        if int[last_index] > 9
-            int[last_index] = int[last_index] % 10
-            next_last_index = last_index - 1
-            int[next_last_index] += 1
+        last_index = int.length - 1 # 2
+        
+        int.each do  |value|
+            int[last_index] = int[last_index] + 1 # 10
+
+            if int[last_index] > 9
+                int[last_index] = int[last_index] % 10 # 10 % 10 = 0
+            else
+                break
+            end
+
+            last_index = last_index - 1
         end
+
+        if last_index == -1
+            int.unshift(1)
+        end
+
         int
     end
 end 
